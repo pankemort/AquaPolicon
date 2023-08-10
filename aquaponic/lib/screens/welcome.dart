ef4d9bae277dc23fd0d2376bbb9a1e595f9ac863
Pratam-Kalligudda <kalliguddapratam642002@gmail.com>
2023-08-10 20:53:56 +0530

    Added Image

--- aquaponic/lib/screens/welcome.dart
+++ aquaponic/lib/screens/welcome.dart
@@ -5,27 +5,71 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      appBar: null,
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.lightBlue,
-            child: Column(
-              children: [
-                const Text("AquaPonic"),
-                const Text(
-                    "Aquaponics is a cooperation between plants and fish and the term originates from the two words aquaculture (the growing of fish in a closed environment) and hydroponics (the growing of plants usually in a soil-less environment). Aquaponic systems come in various sizes from small indoor units to large commercial units"),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("See Live Updates"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Aquaponic"),
+        actions: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Sign Up",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Theme.of(context).colorScheme.onPrimary),
                 ),
-              ],
-            ),
-          ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Sign In"),
+              ),
+            ],
+          )
         ],
       ),
-    ));
+      body: Container(
+        color: Colors.lightBlue.shade50,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Image.asset('assets/images/Aquaponics1.png'),
+            const SizedBox(
+              height: 24,
+            ),
+            RichText(
+              text: const TextSpan(
+                  text: 'Aquaponics ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text:
+                          'is a cooperation between plants and fish and the term originates from the two words aquaculture (the growing of fish in a closed environment) and hydroponics (the growing of plants usually in a soil-less environment). Aquaponic systems come in various sizes from small indoor units to large commercial units',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("See Live Updates"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
